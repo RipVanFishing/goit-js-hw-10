998a8031cded541a2681767a3fd70262035de53a
Denis Serkhel <ripvanfish27@gmail.com>
2022-01-23 21:07:23 +0200

    делаю разметку

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    input: document.querySelector('#search-box'),
+    cardMarkup: document.querySelector('.country-list')
+}
+
+refs.input.addEventListener('input', onSearchInput)
+
+function onSearchInput() {
+
+}
+
+fetch('https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages').then(response => {
+    return response.json()
+}).then(data => console.log(data));
